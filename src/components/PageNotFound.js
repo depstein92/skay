6ea8555bf0906dba6a325af4d7119f51059d89d9
Daniel Epstein <d.epstein92@gmail.com>
2018-08-16 12:34:08 -0400

    added PageNotFound component

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/PageNotFound.scss';
+
+const PageNotFound = () => {
+  return(
+    <div>
+     <div className="page-not-found"></div>
+    </div>
+  )
+}
+
+export default PageNotFound;
--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,8 @@ import Checkout from '../containers/Checkout';
 import Book_Appointment from '../containers/Book_Appointment';
 import { hot } from 'react-hot-loader'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import PageNotFound from './PageNotFound';
 import { persistor, store } from './../configureStore';
 import createBrowserHistory from "history/createBrowserHistory";
 const history = createBrowserHistory();
@@ -17,12 +18,15 @@ class App extends Component {
     return (
     <Router>
       <div>
+      <Switch>
        <Route exact path='/' component={Home_Landing} />
         <Route path='/shop' component={Store} />
         <Route path='/checkout' component={Checkout} />
         <Route path='/about' component={About} />
         <Route path='/appointment' component={Appointment} />
         <Route path='/book_appointment/:day/:month' component={Book_Appointment} />
+        <Route component={PageNotFound} />
+      </Switch>
       </div>
     </Router>
     );
