a6536dbe30b7f69268f0cfcc32dd322f8c65bd9c
Daniel Epstein <d.epstein92@gmail.com>
2018-09-21 19:58:30 -0400

    added absolute path

--- src/components/Landing_Hero_Image.js
+++ src/components/Landing_Hero_Image.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/LandingHeroImage.scss'
+import pineappleImage from '../styles/images/pineapple.jpg';
 
 const Landing_Hero_Image = () => {
   return (
-    <div className="home-landing">
+    <div className="home-landing" style={{ backgroundImage: pineappleImage }}>
      <div className="Skay_Logo">Skay</div>
      <div id="media-icons-landing">
       <button className="ui circular facebook icon button">
